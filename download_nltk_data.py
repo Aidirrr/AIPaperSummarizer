8e7ff90f1459a0ab8a6221a49e60aee5c956f3a5
Aidirrr <khaidhirrazin04@gmail.com>
2024-09-24 05:35:35 +0800

    update app.py

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -9,12 +9,14 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context
 
 def download_nltk_data():
-    nltk.download('punkt')
-    nltk.download('punkt_tab')
-    nltk.download('stopwords')
-    nltk.download('averaged_perceptron_tagger')
-    nltk.download('wordnet')
-    print("NLTK data downloaded successfully.")
+    try:
+        nltk.download('punkt')
+        nltk.download('stopwords')
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('wordnet')
+        print("NLTK data downloaded successfully.")
+    except Exception as e:
+        print(f"An error occurred while downloading NLTK data: {e}")
 
 if __name__ == "__main__":
     download_nltk_data()
